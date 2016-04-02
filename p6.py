5d2cf3c561d41e3f915ea96b2e2c0236a635ffba
Sc00rpion <skarbekm@ee.pw.edu.pl>
2016-04-02 21:54:40 +0200

    z

---
+++ p6.py
@@ -0,0 +1,24 @@
+import sys
+import stribg
+def modularInverse(a, b):
+	x, u, w, z = 0, 1, a, b
+	q, x = 0
+	while w:
+		if w < z:
+			u, x = x, u
+			w, z = z, w
+		q = w // z
+		u = u - q * x
+		w = w - q * z
+	if z != 1:
+		print "no solution"
+		return 0
+	if x < 0:
+		x += b
+	print x
+	return x
+
+l_1 = ibt(sys.argv[1])
+l_2 = ibt(sys.argv[2])
+#28 i 75 ma byc 67
+print modularInverse(l_1, l_2)
