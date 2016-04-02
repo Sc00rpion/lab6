a501d93a1b7c1b6e2ff533eba540284fc44cf62f
Sc00rpion <skarbekm@ee.pw.edu.pl>
2016-04-02 21:13:52 +0200

    z

---
+++ p5.py
@@ -0,0 +1,17 @@
+import sys
+import string
+import p4_euklides
+
+def relativelyPrime(l1, l2):
+	if p4_euklides.nwd( l1, l2 ) == 1:
+		return True
+	else:
+		return False
+	
+
+l1 = int(sys.argv[1])
+l2 = int(sys.argv[2])
+if relativelyPrime(l1, l2):
+	print 'Numbers', l1, "and", l2, 'are relatively prime'
+else:
+	print 'Numbers', l1, 'and', l2, 'aren\'t relatively prime'
