b6c511c1b753eddb3be4304433d6cbc78d31ff1f
Sc00rpion <skarbekm@ee.pw.edu.pl>
2016-04-06 16:16:33 +0200

    z

--- p1.py
+++ p1.py
@@ -11,31 +11,48 @@ def readFile(path):
 def writeFile(text, path):
 	open(path, 'w').write(text)
 
-def lenght():
+def numCharTogether():
 	return 2
-
+	
+def complement0(x):
+	
+	complementTo = 3
+	
+	l = len(x)
+	if l != complementTo:
+		tmp = ''
+		for i in range(complementTo - l):
+			tmp += '0'
+		return tmp + x
+	else
+		return x
+		
+def addSpace(text, number):
+	for x in range(0, number):
+		text += ' '
+	return text
+		
 def txtToNum(text):
 	tab = []
-
-	con = len(text) % lenght()
-	if con != 0:
-		for x in range(0,con):
-			text += ' '
+	con = len(text) % numCharTogether()
+	text = addSpace(text, con)
 	i = 0
 	while i < len(text):
 		tmp =''
-		for x in range(i,lenght()+i):
-			tmp += str(ord(text[x]))
-		tab.append(long(tmp))
-		i += lenght()
+		for x in range(i,numCharTogether()+i):
+			tmp += complement0(str(ord(text[x])))
+		tab.append(tmp)
+		i += numCharTogether()
 	return tab
 	
 def numToTxt(text):
 	result = ''
 	tab = text.split()
 	for x in tab:
-		result += chr(x // 1000)
-		result += chr(x % 1000)
+		i = 0
+		while i < len(x):
+			result += chr(x[i:numCharTogether() + i])
+			i += numCharTogether()
 	return result
 
 def numToString(numbers):
